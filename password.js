9413249946696420c71a3ab79477fdf4d63a7cdf
tobytobi1 <57180694+tobytobi1@users.noreply.github.com>
2020-01-19 21:24:56 -0500

    Add files via upload

---
+++ password.js
@@ -0,0 +1,24 @@
+const name= document.getElementById('name')
+const password= document.getElementById('password')
+const form= document.getElementById('form')
+const errorElement= document.getElementById('error')
+
+form.addEventListener('submit',(e) => {
+	let messages= [];
+	if (name.value === ''|| name.value == null){
+		messages.push('name is required')
+	}
+	if (password.value.length<= 9) {messages.push(' password must be greater than or equal to 10 digits')}
+	if (password.value.length>= 35) {messages.push(' password must be less than or equal to 35 digits')}
+	if (password.value ==='password') {
+		messages.push('password can not be set to password')
+	 var lowerCaseLetters = /[a-z]/g
+  
+	}
+		if (messages.length > 0) {
+	e.preventDefault()
+	errorElement.innerText= messages.join(', ')
+}
+})
+
+
